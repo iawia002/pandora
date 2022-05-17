bc7d185dd395a431243d4d682f38cbf91035d1ba
Xinzhao Xu <z2d@jifangcheng.com>
2022-05-17 18:17:21 +0800

    add generic client

---
+++ kubernetes/client/generic/example/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
+
+	genericclient "github.com/iawia002/pandora/kubernetes/client/generic"
+)
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "kubeconfig",
+				Usage: "kube config file path",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			config, err := clientcmd.BuildConfigFromFlags("", c.String("kubeconfig"))
+			if err != nil {
+				return err
+			}
+			client, err := genericclient.NewClient(config, genericclient.WithContext(context.TODO()))
+			if err != nil {
+				return err
+			}
+			nodes := &corev1.NodeList{}
+			if err = client.List(context.TODO(), nodes); err != nil {
+				return err
+			}
+			for _, node := range nodes.Items {
+				fmt.Println(node.Name)
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
