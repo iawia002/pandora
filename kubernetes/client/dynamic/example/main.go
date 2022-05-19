ce150c77d4fb7c8831042df9f2593601b1efa34c
Xinzhao Xu <z2d@jifangcheng.com>
2022-05-19 16:28:08 +0800

    add dynamic client and unstructured example

---
+++ kubernetes/client/dynamic/example/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+
+	unstructuredutils "github.com/iawia002/pandora/kubernetes/unstructured"
+)
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "kubeconfig",
+				Usage: "kube config file path",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			config, err := clientcmd.BuildConfigFromFlags("", c.String("kubeconfig"))
+			if err != nil {
+				return err
+			}
+			dynamicClient, err := dynamic.NewForConfig(config)
+			if err != nil {
+				return err
+			}
+			objects, err := dynamicClient.Resource(corev1.SchemeGroupVersion.WithResource("nodes")).List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				return err
+			}
+			// metadata map[resourceVersion:91508551]
+			// kind NodeList
+			// apiVersion v1
+			for key, value := range objects.Object {
+				fmt.Println(key, value)
+			}
+			for i := range objects.Items {
+				object := &objects.Items[i]
+				conditions, _, err := unstructured.NestedSlice(object.Object, "status", "conditions")
+				if err != nil {
+					return err
+				}
+				node := &corev1.Node{}
+				if err = unstructuredutils.ConvertToTyped(object, node); err != nil {
+					return err
+				}
+				fmt.Printf("node %s podCIDR: %s, conditions length: %d\n", object.GetName(), node.Spec.PodCIDR, len(conditions))
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ kubernetes/unstructured/unstructured.go
@@ -0,0 +1,26 @@
+package unstructured
+
+import (
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// ConvertToUnstructured converts a typed object to an unstructured object.
+func ConvertToUnstructured(obj interface{}) (*unstructured.Unstructured, error) {
+	uncastObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+	return &unstructured.Unstructured{Object: uncastObj}, nil
+}
+
+// ConvertToTyped converts an unstructured object to a typed object.
+// Usage:
+// 	node := &corev1.Node{}
+// 	ConvertToTyped(object, node)
+func ConvertToTyped(obj *unstructured.Unstructured, typedObj interface{}) error {
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.UnstructuredContent(), typedObj); err != nil {
+		return err
+	}
+	return nil
+}
